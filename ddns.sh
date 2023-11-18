1f09aac265fe5fcb22ad1263bcd80b98a1fd1055
peter-marshall5 <78369157+peter-marshall5@users.noreply.github.com>
2023-11-17 19:43:38 -0500

    Add placeholders and filter secondary address
    btw I regenerated the token that I accidentally leaked last time

--- ddns.sh
+++ ddns.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-token=041e30cd-e1ea-421e-a3f3-fb0105d3ca86
-domain=opcc
+token={YOUR_TOKEN_HERE}
+domain={YOUR_DOMAIN_HERE}
 interval=60
 if=br0
 
 cache=/tmp/ddns.cache
 
 while true; do
-  ip6=$(ip -6 addr show dev ${if} scope global | grep inet6 | cut -d' ' -f6 | cut -d'/' -f1 | grep -v '^f' | head -n 1)
+  ip6=$(ip -6 addr show dev ${if} scope global | grep inet6 | grep -v secondary | cut -d' ' -f6 | cut -d'/' -f1 | grep -v '^f' | head -n 1)
   if [ "$ip6" != "" ]; then
     if [ ! -e "$cache" ] || [ "$ip6" != "$(cat $cache)" ]; then
       echo "$ip6" > "$cache"
